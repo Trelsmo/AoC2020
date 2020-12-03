8a388eda7a871bd41f5a08063cea07fa41eaf094
Svante Trelsmo <svantetrelsmo@gmail.com>
2020-12-03 15:00:00 +0100

    Day 3 solved

---
+++ Day3/p1.py
@@ -0,0 +1,18 @@
+f = open("input.txt","r")
+
+nbr_of_trees = 0
+i=0
+
+for x in f:
+	ind = i % (len(x)-1)
+	if x[ind] == ".":
+		x = x[: ind] + "O" + x[ind + 1:] 
+	elif x[ind] == "#":
+		nbr_of_trees +=1
+		x = x[: ind] + "X" + x[ind + 1:] 
+	i+=3
+
+
+print("Number of trees = ", nbr_of_trees)
+	
+	
---
+++ Day3/p2.py
@@ -0,0 +1,26 @@
+
+down = [1,1,1,1,2]
+steps_right = [1,3,5,7,1]
+mult = 1
+for ind_a in range(len(steps_right)):
+
+	steps = steps_right[ind_a]
+	steps_down = down[ind_a]
+	nbr_of_trees = 0
+	f = open("input.txt","r")
+	i=0
+	mod_var = 1
+
+	for x in f:
+		if steps_down == 2 and (mod_var % 2) == 0:
+			pass	
+		else:
+			ind = i % (len(x)-1)
+			if x[ind] == "#":
+				nbr_of_trees +=1
+			i += steps
+		mod_var += 1
+	mult *= nbr_of_trees
+	print(nbr_of_trees)
+
+print("Multiplied trees = ", mult)
